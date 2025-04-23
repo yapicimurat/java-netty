1de4de08bd494031d8b819ede359e9263cb97e42
Murat YAPICI <muratyapiicii@gmail.com>
2025-04-23 15:37:12 +0300

    Netty documentation discard, time and echo server

---
+++ src/main/java/com/example/DiscardServerHandler.java
@@ -0,0 +1,33 @@
+package com.example;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.ReferenceCountUtil;
+
+public class DiscardServerHandler extends ChannelInboundHandlerAdapter {
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        //discard the incoming message from anywhere
+        //((ByteBuf)msg).release();
+
+//        ByteBuf in = (ByteBuf) msg;
+//        try {
+//            while (in.isReadable()) { // (1)
+//                System.out.print((char) in.readByte());
+//                System.out.flush();
+//            }
+//        } finally {
+//            ReferenceCountUtil.release(msg); // (2)
+//        }
+
+        ctx.writeAndFlush(msg);
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        System.out.println("An exception occurred.");
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
